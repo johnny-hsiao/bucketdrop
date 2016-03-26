f2ba9f6d91494488783d10ac3241c50a580b6dd3
TrevorGuile <trevor.guile@gmail.com>
2016-03-26 22:37:25 +0000

    front end first draft

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,19 @@
 get '/' do
   erb :index
 end
+
+get '/dashboard' do
+  erb :'dashboard/index'
+end
+
+get '/register' do
+  erb :'register/index'
+end
+
+get '/settings' do
+  erb :'settings/index'
+end
+
+get '/redirection' do
+  erb :'register/redirection'
+end
